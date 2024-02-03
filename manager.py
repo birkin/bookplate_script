25f044522bfdee4cd264375a45e7fa97515b0eb2
Birkin James Diana <birkin.diana@gmail.com>
2024-02-03 10:40:41 -0500

    handles `report` or `update` argument.

--- manager.py
+++ manager.py
@@ -1,17 +1,30 @@
+import argparse
+
+
 def manage_report():
     # ...
-    print( 'will generate report' )
+    print('will generate report')
     # ...
     return
+
 
 def manage_daily_db_update():
     # ...
-    print( 'will update daily db' )
+    print('will update daily db')
     # ...
     return
 
 
-## main
 if __name__ == '__main__':
-    manage_report()
-    manage_daily_db_update()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--report', action='store_true', help='run report script')  # the `store_true` action stores `True` if the argument is present, and `False` otherwise
+    parser.add_argument('--update', action='store_true', help='run update script')
+    args = parser.parse_args()
+
+    if args.report:
+        manage_report()
+    elif args.update:
+        manage_daily_db_update()
+    else:
+        manage_report()
+        manage_daily_db_update()
