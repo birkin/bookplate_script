77f7d70a52677ff6a5a1295dcc2773e0c74b24dd
Birkin James Diana <birkin.diana@gmail.com>
2024-02-03 10:35:46 -0500

    stubs structure.

---
+++ manager.py
@@ -0,0 +1,17 @@
+def manage_report():
+    # ...
+    print( 'will generate report' )
+    # ...
+    return
+
+def manage_daily_db_update():
+    # ...
+    print( 'will update daily db' )
+    # ...
+    return
+
+
+## main
+if __name__ == '__main__':
+    manage_report()
+    manage_daily_db_update()
